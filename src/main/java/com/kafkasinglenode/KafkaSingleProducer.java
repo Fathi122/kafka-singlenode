c283be60a9663773f9ba0137f14cca6e4e530fee
fathi122 <abdfatman@gmail.com>
2021-06-19 18:40:54 +0200

    add callback for producer and threadpool for consumer

--- src/main/java/com/kafkasinglenode/KafkaSingleProducer.java
+++ src/main/java/com/kafkasinglenode/KafkaSingleProducer.java
@@ -3,6 +3,7 @@ package com.kafkasinglenode;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.record.CompressionType;
 
 import java.util.Properties;
 import java.util.Random;
@@ -25,14 +26,19 @@ public class KafkaSingleProducer {
         kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringSerializer");
 
+        //Set ACKS to all so all replicas needs to acknolwedge
+        kafkaProps.put(ProducerConfig.ACKS_CONFIG, "all");
+
+        //Set compression type to GZIP
+        kafkaProps.put(ProducerConfig.COMPRESSION_TYPE_CONFIG,
+                CompressionType.GZIP.name);
+
         //Create a Kafka producer from configuration
         KafkaProducer simpleProducer = new KafkaProducer(kafkaProps);
 
         //Publish 10 messages at 1 second intervals, with a random key
         try {
-
             int startKey = (new Random()).nextInt(1000);
-
             for (int i = startKey; i < startKey + 10; i++) {
 
                 //Create a producer Record
@@ -45,8 +51,8 @@ public class KafkaSingleProducer {
 
                 System.out.println("Sending Message : " + kafkaRecord.toString());
 
-                //Publish to Kafka
-                simpleProducer.send(kafkaRecord);
+                //Publish to Kafka with Callback on Producer
+                simpleProducer.send(kafkaRecord, new KafKaProducerCallBack(String.valueOf(i)));
 
                 Thread.sleep(1000);
             }
